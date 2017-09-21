371fd851a475f2ba886d312d1db26aeb6c805135
VOLTTRON <volttron@pnnl.gov>
2017-09-21 09:31:36 -0700

    Bump version number.

--- pnnl/AirsideRCxAgent/airside/airside_aircx.py
+++ pnnl/AirsideRCxAgent/airside/airside_aircx.py
@@ -70,7 +70,7 @@ SA_TEMP_RCX = "Supply-air Temperature Set Point Control Loop Dx"
 SA_TEMP_RCX1 = "Low Supply-air Temperature Dx"
 SA_TEMP_RCX2 = "High Supply-air Temperature Dx"
 dx_list = [DUCT_STC_RCX, DUCT_STC_RCX1, DUCT_STC_RCX2, SA_TEMP_RCX, SA_TEMP_RCX1, SA_TEMP_RCX2]
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 setup_logging()
 _log = logging.getLogger(__name__)
